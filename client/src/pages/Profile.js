e4c6c2dbbd726ec9d1555848e87c0c0f40c390ba
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-21 18:34:55 +0400

    built profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,6 +6,8 @@ import ThoughtList from '../components/ThoughtList';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
+import FriendList from '../components/FriendList';
+
 const Profile = () => {
   const { username: userParam } = useParams();
 
@@ -32,7 +34,13 @@ const Profile = () => {
           <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
         </div>
 
-        <div className="col-12 col-lg-3 mb-3">{/* PRINT FRIEND LIST */}</div>
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
+        </div>
       </div>
     </div>
   );
